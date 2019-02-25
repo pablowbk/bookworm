7360be42cecfd3f9a8b6f1fa696ed09df9148e50
pablowbk <pablo.weisbek@gmail.com>
2019-02-25 07:08:06 -0300

    Updates

---
+++ precache-manifest.3e2af878d45a676bc224db277875bfeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "161ee0e70b6c90f9adc1",
+    "url": "/bookworm/static/js/runtime~main.161ee0e7.js"
+  },
+  {
+    "revision": "bf711c54c8df6ca4fcbe",
+    "url": "/bookworm/static/js/main.bf711c54.chunk.js"
+  },
+  {
+    "revision": "217e7796f8d1d533ff87",
+    "url": "/bookworm/static/js/2.217e7796.chunk.js"
+  },
+  {
+    "revision": "bf711c54c8df6ca4fcbe",
+    "url": "/bookworm/static/css/main.8d87a5df.chunk.css"
+  },
+  {
+    "revision": "e74bf361f9fa5b6858d03c8a8c5c8dd7",
+    "url": "/bookworm/index.html"
+  }
+];
